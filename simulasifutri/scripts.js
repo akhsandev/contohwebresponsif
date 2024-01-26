31d352f74c8d58ea5f3924c1560bd3cf0c97bdb6
akhsandev <97935038+akhsandev@users.noreply.github.com>
2024-01-27 01:27:34 +0800

    a

---
+++ simulasifutri/scripts.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const iframe = document.getElementById('simulation-iframe');
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+   
+    iframe.width = width;
+    iframe.height = height;
+   });
